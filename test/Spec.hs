999b648762fa6ed0a528455ae30e5037729b63a9
ylilarry <ylilarry@gmail.com>
2016-02-21 14:43:35 -0500

    Win10 x64 tested

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import System.Power
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+   describe "System.Power" $ do
+      it "currentPower" $ do
+         currentPower >>= (`shouldBe` Default)
+      it "requirePower" $ do
+         suggestPower RequireDisplayAndSystem
+         currentPower >>= (`shouldBe` RequireDisplayAndSystem)
+         currentPower >>= (`shouldBe` RequireDisplayAndSystem)
+         suggestPower RequireDisplay
+         currentPower >>= (`shouldBe` RequireDisplay)
+         currentPower >>= (`shouldBe` RequireDisplay)
+         suggestPower RequireSystem
+         currentPower >>= (`shouldBe` RequireSystem)
+         currentPower >>= (`shouldBe` RequireSystem)
